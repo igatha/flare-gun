5de2e13df0cb1932a209c882880108812a877c3b
Nizar <nizarmah@hotmail.com>
2024-10-05 16:40:25 +0300

    Initial Commit

---
+++ Igatha/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Igatha
+//
+//  Created by Nizar Mahmoud on 05/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Igatha/IgathaApp.swift
@@ -0,0 +1,17 @@
+//
+//  IgathaApp.swift
+//  Igatha
+//
+//  Created by Nizar Mahmoud on 05/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct IgathaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
