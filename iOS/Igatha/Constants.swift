d6fcc05cb51c1537d8960daac9fe2dc6c7192b17
Nizar <nizarmah@hotmail.com>
2024-11-01 12:59:42 +0200

    ios: Update bundle and signature
    1. Adds Apple developer account signature
    2. Updates bundle to com.nizarmah.igatha

--- iOS/Igatha/Constants.swift
+++ iOS/Igatha/Constants.swift
@@ -9,8 +9,8 @@ import CoreBluetooth
 
 struct Constants {
     static let SOSBeaconServiceID: CBUUID = CBUUID(string: "1802")
-    static let SOSBeaconRestoreID: String = "me.nizarmah.Igatha.sosbeacon"
-    
+    static let SOSBeaconRestoreID: String = "com.nizarmah.igatha.sosbeacon"
+
     // threshold for sudden changes in linear acceleration
     // 3.0 g ~= dropping your phone on a hard surface
     static let SensorAccelerationThreshold: Double = 3.0
@@ -20,7 +20,7 @@ struct Constants {
     // threshold for sudden changes in atmospheric pressure
     // 0.1 kPa ~= altitude change of approx. 8 to 12 meters
     static let SensorPressureThreshold: Double = 0.1
-    
+
     // key for disaster detector enabled setting in app storage
     static let DisasterDetectionSettingsKey: String = "disasterDetectionEnabled"
     // time window for temporally correlating sensor readings
@@ -29,7 +29,7 @@ struct Constants {
     // grace period (seconds) before an incident response is triggered
     static let DisasterResponseGracePeriod: Double = 120.0
     static let DisasterResponseNotificationID: String = "DISASTER_RESPONSE"
-    
+
     // percentage of how much a new value should affect the old value
     static let RSSIExponentialMovingAverageSmoothingFactor: Double = 0.18
 }
